eb986f5cabf7dfa174df2980907de50212dd28aa
Sergey Shliakhov <husband.sergey@gmail.com>
2020-01-20 09:11:33 +0100

    Add build

---
+++ build/dav/tag.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Tag {
+    constructor(id, name) {
+        this._id = id;
+        this._name = name;
+    }
+    id() {
+        return this._id;
+    }
+    name() {
+        return this._name;
+    }
+}
+exports.Tag = Tag;
+//# sourceMappingURL=tag.js.map
---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var client_1 = require("./dav/client");
+exports.Client = client_1.Client;
+var tag_1 = require("./dav/tag");
+exports.Tag = tag_1.Tag;
+var fileprops_1 = require("./dav/fileprops");
+exports.FileProps = fileprops_1.FileProps;
+//# sourceMappingURL=index.js.map
