40da71afbc9a9c9980be129183da5824f6d42c97
Tim Loewel <tim.loewel@wuerth-it.com>
2020-02-05 19:25:35 +0100

    ignore build dir

--- build/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("./dav/client");
-exports.Client = client_1.Client;
-var tag_1 = require("./dav/tag");
-exports.Tag = tag_1.Tag;
-var fileProps_1 = require("./dav/fileProps");
-exports.FileProps = fileProps_1.FileProps;
-//# sourceMappingURL=index.js.map
